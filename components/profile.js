626eda11d3ca7d5fd1fb62b5e9fb157b73fbfd21
Adem CILGIN <acilgin@netrd.com.tr>
2021-05-06 01:41:23 +0300

    frame

--- components/profile.js
+++ components/profile.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default  function Profile() {
     return <figure className=" md:flex bg-gray-200 rounded-xl p-8 md:p-0 shadow-2xl  site-text-main dark:bg-gray-800 ">
         <img className="w-32 h-32 md:w-48 md:h-auto md:rounded-full rounded-full mx-auto shadow-2xl" src="adem_cilgin.jpg" alt=""
@@ -16,6 +18,12 @@ export default  function Profile() {
                     </div>
                     <div className="text-gray-500">
                         Staff Engineer, Turkey
+                    </div>
+                    <div className="text-gray-50 b  justify-center items-center w-1/4">
+                        <Link href="/room" >
+                                <button className="bg-pink-500 hover:bg-pink-400 w-auto justify-center items-center text-center w-auto  rounded-xl mt-2 p-3"> Join Chat With Me </button>  
+                            
+                        </Link>
                     </div>
                 </figcaption>
             </div>
--- pages/room/index.js
+++ pages/room/index.js
@@ -1,12 +1,13 @@
 export default function RoomPage() {
   return <>
-    <div className="site-container">
-
-      <iframe className="w-full h-full 2xl:max-h-full"
+    <div className="max-w-5xl mx-auto px-6">
+    
+    <iframe  className="min-w-full min-h-full  mx-auto h-3/4"
         src="https://aclgn.whereby.com/4daf4455-9cd5-4916-bd6d-bd1fb86a7bc6?embed"
         allow="camera; microphone; fullscreen; speaker; display-capture"
       ></iframe>
-
+    
+  
     </div>
   </>
 
