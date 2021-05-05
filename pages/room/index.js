811192acb60b5403f5c5c0927160958d99d0c055
Adem CILGIN <acilgin@netrd.com.tr>
2021-05-06 00:58:56 +0300

    added iframe

---
+++ pages/room/index.js
@@ -0,0 +1,13 @@
+export default function RoomPage() {
+  return <>
+    <div className="site-container">
+
+      <iframe className="w-full h-full 2xl:max-h-full"
+        src="https://aclgn.whereby.com/4daf4455-9cd5-4916-bd6d-bd1fb86a7bc6?embed"
+        allow="camera; microphone; fullscreen; speaker; display-capture"
+      ></iframe>
+
+    </div>
+  </>
+
+}
