509b04d1f7824ac49e6b74e84ae7df602cfb87b9
Adem CILGIN <acilgin@netrd.com.tr>
2021-05-06 11:45:08 +0300

    screenshare

--- pages/room/index.js
+++ pages/room/index.js
@@ -3,7 +3,7 @@ export default function RoomPage() {
     <div className="mx-auto  flex-nowrap bg-local md:bg-fixed">
      <div className="h-full  flex-nowrap h-screen  w-full mb-24 px-10   ">
     <iframe  className="mx-auto  border-gray-700 rounded-md  h-full w-full mx-auto "
-        src="https://aclgn.whereby.com/4daf4455-9cd5-4916-bd6d-bd1fb86a7bc6?embed"
+        src="https://aclgn.whereby.com/4daf4455-9cd5-4916-bd6d-bd1fb86a7bc6?embed&chat=on&screenshare=on"
         allow="camera; microphone; fullscreen; speaker; display-capture"
       ></iframe>
     </div>
